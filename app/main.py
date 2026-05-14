58ca7192bf6ef5e95969335c5cfc55faa71e6411
Shashankc06 <160994288+Shashankc06@users.noreply.github.com>
2026-05-14 22:49:22 +0530

    restore main

--- app/main.py
+++ app/main.py
@@ -1,19 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatRequest(BaseModel):
+    messages: List[Message]
+
 
 @app.get("/")
 def home():
     return {"message": "SHL Assessment Chatbot Running"}
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
+
 @app.post("/chat")
-def chat():
+def chat(request: ChatRequest):
+
+    user_message = request.messages[-1].content
+
     return {
-        "reply": "Chat endpoint working",
+        "reply": f"You said: {user_message}",
         "recommendations": [],
         "end_of_conversation": False
     }
